4f451d6934a42cc29ad79dcd9340536f4e7a4ff0
Antonios Papadakis <1001545@isd535.org>
2020-04-16 10:11:55 -0500

    update signup.php to be functional.

--- signup.php
+++ signup.php
@@ -1,6 +1,75 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<link rel="stylesheet" src="style.css">
+<?php
+//signup.php
+include 'connected.php';
+include 'header.php';
+require 'loginCheck.php';
+ 
+echo '<h3>Sign up</h3>';
+ 
+if($_SERVER['REQUEST_METHOD'] != 'POST')
+{
+    /*the form hasn't been posted yet, display it
+      note that the action="" will cause the form to post to the same page it is on */
+    echo '<form method="post" align="center">
+        Full Name: <input type="text" name="name" class="form-control" placeholder="John Smith" required>
+        Password: <input type="password" class="form-control" placeholder="mypass123" name="pass" required>
+        Phone Number: <input type="number" class="form-control" placeholder="1234567890" name="phone_num" required>
+        <input type="submit" value="Complete Registration" required>
+        </form>';
+}
+else
+{
+    /* so, the form has been posted, we'll process the data in three steps:
+        1.  Check the data
+        2.  Let the user refill the wrong fields (if necessary)
+        3.  Save the data 
+    */
+    $errors = array(); /* declare the array for later use */
+     
+    if(isset($_POST['name']))
+    {
+        //the user name exists
+        if(!ctype_alnum($_POST['name']))
+        {
+            $errors[] = 'The username can only contain letters and digits.';
+        }
+        if(strlen($_POST['name']) > 255)
+        {
+            $errors[] = 'The username cannot be longer than 255 characters.';
+        }
+    }
+
+    if(!empty($errors)) /*check for an empty array, if there are errors, they're in this array (note the ! operator)*/
+    {
+        echo 'Uh-oh.. a couple of fields are not filled in correctly..';
+        echo '<ul>';
+        foreach($errors as $key => $value) /* walk through the array so all the errors get displayed */
+        {
+            echo '<li>' . $value . '</li>'; /* this generates a nice error list */
+        }
+        echo '</ul>';
+    }
+    else
+    {
+        //the form has been posted without, so save it
+
+        $sql = "INSERT INTO users (name, phone_num, password) VALUES (:name, :phone_num, :pass)";
+        
+        $sqlPrep = $connected->prepare($sql);
+        $result = $sqlPrep->execute();
+        if(!$result)
+        {
+            //something went wrong, display the error
+            echo 'Something went wrong while registering. Please try again later.';
+            echo $conected->errorInfo(); //debugging purposes, uncomment when needed
+        }
+        else
+        {
+            echo 'Successfully registered. You can now <a href="signin.php">sign in</a> and start posting! :-)';
+        }
+    }
+}
+ 
+include 'footer.php';
+?>
 
