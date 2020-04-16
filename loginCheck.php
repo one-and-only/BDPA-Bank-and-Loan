d9300bac02f78ecb8e666e0d4d209de72941deb0
Antonios Papadakis <1001545@isd535.org>
2020-04-16 10:44:04 -0500

    comment out all code to check for errors.

--- loginCheck.php
+++ loginCheck.php
@@ -1,13 +1,15 @@
 <?php
-require 'connected.php';
+//require 'index.php';
     
     // At the top of the page check to see whether the user is logged in or not
-    if(empty($_SESSION['name']))
-    {
+  //  if(empty($_SESSION['name']))
+   // {
         // If they are not, redirect them to the login page.
-        header("Location: login.php");
+     //   header("Location: login.php");
         
         // Remember that this die statement is absolutely critical.  Without it,
         // people can view your members-only content without logging in.
-        die('User '.$_SESSION['name'].' Authenticated');
-    }
+       // die('User '.$_SESSION['name'].' Authenticated');
+   // }
+?>
+    
